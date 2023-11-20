7ca7d34b142765cd7a3f8fdfffaf1f2eb571e978
Allen Russell <garyrusselljr@gmail.com>
2023-11-20 13:59:13 -0700

    docs: README and skipped CSRF token auth to test response

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+artist = Artist.create!(name: "Bill", style: "Pop", bio: "I'm a singer", password: "123")
