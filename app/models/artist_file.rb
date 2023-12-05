8c3117022627ecad157bead5c82a1e5029ecfbd0
Allen Russell <garyrusselljr@gmail.com>
2023-12-05 15:07:57 -0700

    feat: ai prompt takes 'style' param and view logic updated for show pages

--- app/models/artist_file.rb
+++ app/models/artist_file.rb
@@ -2,5 +2,12 @@ class ArtistFile < ApplicationRecord
   belongs_to :artist
 
   validates_presence_of :image_url, :resources, :goals, :action_steps
+
   has_one_attached :saved_image
+
+  def update_style(new_style)
+    artist.update(style: new_style)
+
+    update(style: new_style)
+  end
 end
